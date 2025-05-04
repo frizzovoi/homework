b14026f55e091f27dc6931db93343f4864ab35b5
frizzovoi <59707251+frizzovoi@users.noreply.github.com>
2025-05-04 20:16:36 +0300

    Add files via upload

--- !wake-bake/js/main.js
+++ !wake-bake/js/main.js
@@ -19,3 +19,37 @@
     }
 
 })()
+
+// =====
+
+const modal = document.querySelector('.modal')
+const modalBtn = document.querySelector('.about__img-button')
+const body = document.body
+
+const openModal = () => {
+    modal.classList.add('modal--opened')
+    body.classList.add('body--modal-opened')
+}
+
+const closeModal = () => {
+    modal.classList.remove('modal--opened')
+    body.classList.remove('body--modal-opened')
+}
+
+modalBtn.addEventListener('click', event => {
+    event.preventDefault()
+    openModal()
+})
+
+modal.addEventListener('click', event => {
+    if (event.target === modal || event.target.closest('.modal__cancel')) {
+        event.preventDefault()
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape' && modal.classList.contains('modal--opened')) {
+        closeModal()
+    }
+})
