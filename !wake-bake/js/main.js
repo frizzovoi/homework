045071849246bb0c43262da2f12567914dfb2f08
frizzovoi <59707251+frizzovoi@users.noreply.github.com>
2025-05-13 20:51:53 +0300

    Add files via upload

--- !wake-bake/js/main.js
+++ !wake-bake/js/main.js
@@ -178,3 +178,9 @@ new Swiper('.testimonials__slider', {
         },
     }
 });
+
+// Маска для телефона 
+
+const telInputs = document.querySelectorAll('input[type="tel"]')
+const im = new Inputmask('+7 (999) 999-99-99')
+im.mask(telInputs);
